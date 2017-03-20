eae87fbedd1325de27c4713440bbdcc625583bed
jpancake <jrazon_18@yahoo.com>
2017-03-19 20:33:36 -0700

    added delete /todo/:id testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -78,7 +78,7 @@ describe('GET /todos', () => {
   })
 })
 
-describe('GET /todos:id', () => {
+describe('GET /todos/:id', () => {
   it('should return todo doc', () => {
     request(app)
         .get(`/todos/${todos[1]._id.toHexString()}`)
@@ -98,5 +98,36 @@ describe('GET /todos:id', () => {
         .get(`/todos/123`)
         .expect(404)
         .end(done)
+  })
+})
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    const hexId = todos[1]._id.toHexString()
+    request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect(res => expect(res.body.todo._id).toBe(hexId))
+        .end((err, res) => {
+          if (err) {
+            return done(err)
+          }
+          Todo.findById(hexId).then(todo => {
+            expect(todo).toNotExist()
+            done()
+          }).catch(e => done(e))
+        })
+  })
+  it('should return 404 if todo not found', done => {
+    request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done)
+  })
+  it('should return 4040 for non-object ids', done => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done)
   })
 })
