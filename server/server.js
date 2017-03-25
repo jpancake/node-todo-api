b8e9719b66bc45b3865678e5d54beb88660698c3
jpancake <jrazon_18@yahoo.com>
2017-03-25 11:39:22 -0700

    Added POST /user/login

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
+const bcrypt = require('bcryptjs')
 const _ = require('lodash')
 
 const { mongoose } = require('./db/mongoose')
@@ -101,6 +102,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user)
+})
+
+app.post('/users/login', (req, res) => {
+	const body = _.pick(req.body, ['email', 'password'])
+
+	User.findByCredentials(body.email, body.password)
+			.then(user => user.generateAuthToken()
+					.then(token => res.header('x-auth', token)
+							.send(user)))
+							.catch(e => res.status(400)
+								.send())
+
 })
 
 app.listen(3000, () => {
